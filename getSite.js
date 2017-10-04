4ab73a3386f1d20c9619ad021ddf295d72579b7a
Gopi Raja <gopi@inkmonk.com>
2017-10-05 01:01:29 +0530

    enable scripts

--- getSite.js
+++ getSite.js
@@ -5,23 +5,23 @@ class HomePage extends React.Component {
   render() {
     const { manifest } = this.props;
 
-    // const fcSettingsString = `
-    //   const fcSettings = {
-    //         token: 'db46f4af-1e74-450d-bece-14bfee79b038',
-    //         host: 'https://wchat.freshchat.com',
-    //         siteId: 'Home Page',
-    //       };
-    //   `;
+    const fcSettingsString = `
+      const fcSettings = {
+            token: 'db46f4af-1e74-450d-bece-14bfee79b038',
+            host: 'https://wchat.freshchat.com',
+            siteId: 'Home Page',
+          };
+      `;
 
-    // const gaString = `
-    //   window.dataLayer = window.dataLayer || [];
-    //   function gtag() {
-    //     dataLayer.push(arguments);
-    //   }
-    //   gtag('js', new Date());
-    //
-    //   gtag('config', 'UA-107210137-1');
-    // `;
+    const gaString = `
+      window.dataLayer = window.dataLayer || [];
+      function gtag() {
+        dataLayer.push(arguments);
+      }
+      gtag('js', new Date());
+
+      gtag('config', 'UA-107210137-1');
+    `;
     return (
       <html lang="en">
         <head>
@@ -64,13 +64,13 @@ class HomePage extends React.Component {
           />
           <script src={`https://cdn.itsmybio.me/${manifest['main.js']}`} />
 
-          {/* <script dangerouslySetInnerHTML={{ __html: fcSettingsString }} />
-          <script src="https://wchat.freshchat.com/js/widget.js" async /> */}
-          {/* <script
+          <script dangerouslySetInnerHTML={{ __html: fcSettingsString }} />
+          <script src="https://wchat.freshchat.com/js/widget.js" async />
+          <script
             async
             src="https://www.googletagmanager.com/gtag/js?id=UA-107210137-1"
           />
-          <script dangerouslySetInnerHTML={{ __html: gaString }} /> */}
+          <script dangerouslySetInnerHTML={{ __html: gaString }} />
         </body>
       </html>
     );
@@ -81,15 +81,15 @@ class Site extends React.Component {
   render() {
     const { manifest, username, dpUrl, userData } = this.props;
 
-    // const gaString = `
-    //   window.dataLayer = window.dataLayer || [];
-    //   function gtag() {
-    //     dataLayer.push(arguments);
-    //   }
-    //   gtag('js', new Date());
-    //
-    //   gtag('config', 'UA-107210137-1');
-    // `;
+    const gaString = `
+      window.dataLayer = window.dataLayer || [];
+      function gtag() {
+        dataLayer.push(arguments);
+      }
+      gtag('js', new Date());
+
+      gtag('config', 'UA-107210137-1');
+    `;
     return (
       <html lang="en">
         <head>
@@ -130,11 +130,11 @@ class Site extends React.Component {
           />
           <script src={`https://cdn.itsmybio.me/${manifest['main.js']}`} />
 
-          {/* <script
+          <script
             async
             src="https://www.googletagmanager.com/gtag/js?id=UA-107210137-1"
           />
-          <script dangerouslySetInnerHTML={{ __html: gaString }} /> */}
+          <script dangerouslySetInnerHTML={{ __html: gaString }} />
         </body>
       </html>
     );
